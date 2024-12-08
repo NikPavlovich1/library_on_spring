da2ec8fdb416145c54f9efaf02753a5885f91fc4
Python <iden199119@gmail.com>
2024-12-08 15:30:48 +0300

    created DB

---
+++ src/main/java/org/example/library_on_spring/LibraryOnSpringApplication.java
@@ -0,0 +1,13 @@
+package org.example.library_on_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryOnSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryOnSpringApplication.class, args);
+    }
+
+}
